d4d5ea2b2e4892235e466e383b4ae1360562c115
InnPatron <randomcivvy0121@gmail.com>
2023-03-17 03:21:10 -0700

    Set up paint brush slot ops and keymap

--- __init__.py
+++ __init__.py
@@ -7,19 +7,29 @@ bl_info = {
     "category": "3D View",
 }
 
+paint_brush_slot_count = 10
+registered = []
+keymap_items = []
+
 def make_paint_brush_op_idname(i):
     return "quick_brush.paint_brush_slot{x}".format(x=i)
 
-def paint_brush_slot_factory(n):
-    r = []
+def register_paint_brush_slot_ops(n):
     for i in range(0, n):
-        class QuickBrushPaintBrushSlotOp(object):
-            bl_idname = make_paint_brush_op_idname(i)
-            bl_label = "Quick Brush Paint Brush Slot #{x}".format(x=i)
 
-        r.append(QuickBrushPaintBrushSlotOp)
+        def execute(self, context):
+            print("Slot op {x} in mode {mode}".format(x=self.slot, mode=bpy.context.mode))
+            return {'FINISHED'}
 
-    return r
+        op_type = type("QuickBrushPaintBrushSlot{x}Op".format(x=i), (bpy.types.Operator, ), {
+            "bl_idname": make_paint_brush_op_idname(i),
+            "bl_label": "Quick Brush: Paint Brush Slot {x}".format(x=i),
+            "slot": i,
+            "execute": execute,
+        })
+
+        bpy.utils.register_class(op_type)
+        registered.append(op_type)
 
 # source: https://blenderartists.org/t/how-to-create-a-shortcut-for-a-custom-script/545177
 def register_keymaps(paint_brush_slots):
@@ -27,10 +37,11 @@ def register_keymaps(paint_brush_slots):
     if kc:
         # Register texture paint bindings
         if "Image Paint" not in kc.keymaps:
-            kc.keymaps.new(name="Image Paint", space_type="VIEW_3D")
+            kc.keymaps.new(name="Image Paint", space_type="EMPTY")
         km = kc.keymaps["Image Paint"]
         for i in range(0, paint_brush_slots):
-            kmi = km.keymap_items.new(make_paint_brush_op_idname(i), '', '')
+            kmi = km.keymap_items.new(make_paint_brush_op_idname(i), 'NONE', 'PRESS')
+            keymap_items.append(kmi)
 
 def unregister_keymaps():
     kc = bpy.context.window_manager.keyconfigs.addon
@@ -38,18 +49,21 @@ def unregister_keymaps():
         # Unregister texture paint bindings
         km = kc.keymaps["Image Paint"]
         for kmi in km.keymap_items:
-            if "quick_brush" in kmi.idname:
+            if kmi in keymap_items:
                 km.keymap_items.remove(kmi)
-                break
 
-paint_binding_count = 5
+def unregister_ops():
+    for r in registered:
+        bpy.utils.unregister_class(r)
 
 def register():
+    register_paint_brush_slot_ops(paint_brush_slot_count)
     # bpy.types.BlendData.quick_brush_data = bpy.props.PointerProperty(type=foo)
-    register_keymaps()
+    register_keymaps(paint_brush_slot_count)
 
 def unregister():
     unregister_keymaps()
+    unregister_ops()
 
 if __name__ == "__main__":
     register()
